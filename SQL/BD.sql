1fcc464646d5ca154f649777d4b31adc06ca8a36
Cauã Maia Muniz de Alencar <munizmaia58@gmail.com>
2026-07-01 21:09:02 -0300

    Create initial database and contas table

---
+++ SQL/BD.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE sistema_projetofinalBD2;
+
+USE sistema_projetofinalBD2;
+
+CREATE TABLE contas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    saldo DECIMAL(10,2)
+);
+
+INSERT INTO contas (nome, saldo)
+VALUES
+('SEU_NOME_EXEMPLO1',1000),
+('SEU_NOME_EXEMPLO2',1000);
